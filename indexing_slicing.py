bdde5c27ba0098bc6fdb459905a05fca145374f4
Stephen Cleary <steve0cleary@gmail.com>
2023-02-01 14:49:13 +0000

    first commit

---
+++ indexing_slicing.py
@@ -0,0 +1,13 @@
+#String exercise for indeing and slicing python strings
+
+message = input("Enter a message: ")
+
+print("First character:", message[0])
+print("Last character:", message[-1])
+print("Middle character:", message[int(len(message) / 2)])
+print("Even index characters:", message[0::2])
+print("Odd index characters:", message[1::2])
+print("Reverse message:", message[::-1])
+
+
+

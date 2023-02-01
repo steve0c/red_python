6f72ff9ba7bde244451fe63dc6049b48732cdd2b
Stephen Cleary <steve0cleary@gmail.com>
2023-02-01 14:48:44 +0000

    first commit

---
+++ mini-project-list.py
@@ -0,0 +1,41 @@
+#This is a mini project to practice creating lists
+
+#Create an empty list
+
+my_list = []
+
+#Populate the list using append or insert
+
+my_list.append("S3")
+my_list.append("EC2")
+my_list.append("Cloudfront")
+my_list.append("Lambda")
+my_list.append("RDS")
+my_list.append("DynamoDB")
+
+#Print the list and the length of the list
+
+print(my_list)
+print(len(my_list))
+
+#Remove two specific services from the list by name or by index
+
+my_list.remove("RDS")
+my_list.remove("S3")
+
+#Print the new list and the length of the list
+
+print(my_list)
+print(len(my_list))
+
+
+
+
+
+
+
+
+
+
+
+

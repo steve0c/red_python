1accd27322bfd83c713838f1fc4f797acc947872
Stephen Cleary <steve0cleary@gmail.com>
2023-02-02 13:10:09 +0000

    first commit

---
+++ spliting-lab.py
@@ -0,0 +1,70 @@
+# learning how to split 
+
+#line = "This is a string"
+ 
+#print(line.split())
+#print(line.split(' '))
+
+#print('-'.join(line.split(' ')))
+
+#line = line.split(" ")
+#line = "-".join(line)
+#print(line)
+
+
+#var = "If there is a will there is a way"
+
+#var = var.split()
+#var= "/".join(line)
+
+#print(var)
+
+#steve = "starting to get a hang of this split thing"
+
+#steve = steve.split(" ")
+#steve = "$".join(steve)
+#print(steve)
+
+#cando = "Want me to split a string and then join it with a - ?"
+#print(cando)
+
+#cando = cando.split()
+#print(cando)
+#cando = "-".join(cando)
+#print(cando)
+
+#park = '-'.join(park)
+
+#print(park)
+
+#today = "Is goin to be a good day"
+#today = today.split()
+#today = "-".join(today)
+#print(today)
+
+#wisconsin = "Really cold this time of year"
+#wisconsin = wisconsin.split()
+#wisconsin = '-'.join(wisconsin)
+#print(wisconsin)
+
+#france = "its nice and warm this time of year"
+#france = france.split()
+#france = '-'.join(france)
+#print(france)
+
+last_one = "This is my last one as I think I've done it enough times to remember"
+
+last_one = last_one.split() 
+print(last_one) #print
+
+last_one = "-".join(last_one) 
+print(last_one)
+
+
+
+
+
+
+
+
+

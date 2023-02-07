747bf3862c1343eaeb43fafd85c96f608eea7772
Stephen Cleary <steve0cleary@gmail.com>
2023-02-07 16:54:39 +0000

    for loop

---
+++ for_loop.py
@@ -0,0 +1,22 @@
+#colors = ['blue', 'green', 'red', 'purple']
+#for color in colors:
+    #print(color)
+    
+    #tuple for loop
+    #point = (1, 2, 3)
+    #for value in point:
+        #print(point)
+        
+        #dictionary for loop
+    
+#ages = {'kevin': 59, 'bob': 40, 'steve': 33}
+#for key in ages:
+    #print(key)
+    
+#for key, value in ages.items():
+    #print(key, value)
+    
+    #string for loop
+    
+#for letter in 'my_string':
+   # print(letter)

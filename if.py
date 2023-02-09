5934fb1186338b4139b4d539eea29676d3927b62
Stephen Cleary <steve0cleary@gmail.com>
2023-02-09 11:57:53 +0000

    if elif else

--- if.py
+++ if.py
@@ -1,6 +1,28 @@
-n = 7 
+#n = 7 
 
-if n <= 7:
-    print("yes")
+#if n <= 7:
+    #print("yes")
+#else:
+    #print("It's not")
+    
+    
+    
+#num1 = 100
+#num2 = 100
+
+#if num1 > num2:
+    #print("Num1 is bigger than Num2")
+#elif num2 > num1:
+    #print("Num2 is bigger than Num1")
+#else: 
+    #print("both numbers are equal")
+    
+one = 30
+two = 30
+    
+if one < two:
+    print("1 is less than two")
+elif two > one:
+    print("TWO is greater than one")
 else:
-    print("It's not")
+    print("they are equal")

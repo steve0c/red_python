7d68d9cf2ff5a4aa68c399874a2cecc4ad451153
Stephen Cleary <steve0cleary@gmail.com>
2023-02-10 09:28:30 +0000

    first commit

---
+++ glob.py
@@ -0,0 +1,4 @@
+#!/usr/bin/pythoimport glob
+import glob
+for f in glob.iglob("*"): 
+    print(f)
--- travis2.py
+++ travis2.py
@@ -1,4 +1,4 @@
-#Thisx is a project to check if a name is on a list
+#This is a project to check if a name is on a list
 
 known_users = ["Alice", "Bob", "Claire", "Dan", "Emma", "Fred", "Georgie", "Harry"]
 

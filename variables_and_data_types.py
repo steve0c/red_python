a20c012bc632627b5851da7c41857702694f74ec
Stephen Cleary <steve0cleary@gmail.com>
2023-02-13 13:36:07 +0000

    variables and data types

---
+++ variables_and_data_types.py
@@ -0,0 +1,43 @@
+#a_str = "This is an example of a string in quotes" # In python the word string is abbreviated to str
+#my_float = 5.5 #a float had a decimal
+#an_integer = 5 # integer is abbreviated to int
+#shopping_list = ["apples", "oranges", "pears"]
+#a_dict = {"userId": "JBloggs"} # dictionary is abbreviated to dict
+#my_var = another_variable # variable is abbreviated to var
+#test_function = my_function() #function is abbreviated to func
+#example_tuple = ("apple", "orange", "pear")
+#boolean_values = True # boolean is abbreviated to bool
+
+#test = "This is my string variable"
+
+#my_int = 50
+#sentence = "The total comes to "
+
+#print(sentence + str(my_int))
+
+#user = {"first_name":"Bob"}
+#print(user)
+
+#print(user["first_name"])
+
+#add key
+
+#user["family_name"]= "Clark"
+#print(user)
+
+#modify value
+
+#user["family_name"]= "Cleary"
+#print(user)
+
+#del key value pair
+
+#del user ["family_name"]
+#print(user)
+
+#lists 
+
+
+
+
+

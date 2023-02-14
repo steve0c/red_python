df2fde61c9bfaeafb3e21b7cb3038e11f906e401
Stephen Cleary <steve0cleary@gmail.com>
2023-02-14 16:16:55 +0000

    creating s3 bucket

--- creating_s3_bucket.py
+++ creating_s3_bucket.py
@@ -1,13 +1,12 @@
 import boto3
 aws_resource=boto3.resource("s3")
-bucket=aws_resource.Bucket("labbucket0560440")
+bucket=aws_resource.Bucket("labbucket05604e40")
 
 response = bucket.create(
     ACL='public-read',
-    CreateBucketConfiguration={
-        'LocationConstraint': 'us-east-2'
-    },
-    
-)
+    CreateBucketConfiguration= {
+        'LocationConstraint':  
+},
+    )
 
 print(response)

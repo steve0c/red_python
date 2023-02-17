3b876b288e3d0b37f6345ac1572c6c16f8f66dc8
Stephen Cleary <steve0cleary@gmail.com>
2023-02-17 09:40:52 +0000

    positional arguments

---
+++ positional_arguments.py
@@ -0,0 +1,17 @@
+import boto3
+
+def translate_text():
+    client = boto3.client('translate')
+    response = client.translate_text(
+        Text='I am learning to code in AWS', 
+        SourceLanguageCode='en', 
+        TargetLanguageCode='fr' 
+    )
+    print(response) 
+
+def main():
+    translate_text()
+
+if __name__=="__main__":
+    main()
+
